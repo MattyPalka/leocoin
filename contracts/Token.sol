b4977aec518b10f4cbd580d3f45b5e05d7e7b00a
Mateusz Pałka <mateusz.palka@leocode.com>
2022-02-02 22:52:09 +0100

    moved to pure functions

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,19 +3,12 @@ pragma solidity ^0.8.0;
 
 contract Token {
 
-  uint private _totalSupply;
-  string private _tokenName;
-  string private _tokenSymbol;
-  uint8 private _decimals;
   address public owner;
   mapping (address => uint) private balances;
-  mapping (address => mapping (address => uint)) private _allowance;
+  mapping (address => mapping (address => uint)) private allowed;
 
   constructor(){
-    _totalSupply = 100_000;
-    _tokenSymbol = "LEO";
-    _tokenName = "Leocode Token";
-    _decimals = 18;
+
     balances[msg.sender] = totalSupply();
     owner = msg.sender;
   }
@@ -24,20 +17,20 @@ contract Token {
 
   event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-  function name() public view returns (string memory){
-    return _tokenName;
+  function name() public pure returns (string memory){
+    return "Leocode Token";
   }
 
-  function symbol() public view returns (string memory){
-    return _tokenSymbol;
+  function symbol() public pure returns (string memory){
+    return "LEO";
   }
 
-  function decimals() public view returns (uint8){
-    return _decimals;
+  function decimals() public pure returns (uint8){
+    return 18;
   }
 
-  function totalSupply() public view returns (uint256){
-    return _totalSupply * ( 10 ** decimals());
+  function totalSupply() public pure returns (uint256){
+    return 100_000 * ( 10 ** decimals());
   }
 
   function balanceOf(address _owner) public view returns (uint256 balance){
@@ -60,7 +53,7 @@ contract Token {
   function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
     require(_to != address(0), "Cannot transfer to zero address");
     require(_from != address(0), "Cannot spend from zero address");
-    require(balances[_from] >= _value && _allowance[_from][msg.sender] >= _value, "Insufficient funds");
+    require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value, "Insufficient funds");
     
     balances[_to] += _value;
     balances[_from] -= _value;
@@ -73,7 +66,7 @@ contract Token {
   function approve(address _spender, uint256 _value) public returns (bool success){
     require(_spender != address(0), "Cannot spend from zero address");
     
-    _allowance[msg.sender][_spender] = _value;
+    allowed[msg.sender][_spender] = _value;
     
     emit Approval(msg.sender, _spender, _value);
 
@@ -81,6 +74,6 @@ contract Token {
   }
 
   function allowance(address _owner, address _spender) public view returns (uint256 remaining){
-    return _allowance[_owner][_spender];
+    return allowed[_owner][_spender];
   }
 }
