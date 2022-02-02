bc80c3a52a6d3fe9c5d1a5ad48b3c3fe437ecb14
Mateusz Pałka <mateusz.palka@leocode.com>
2022-02-02 21:12:29 +0100

    Add:  - deployment
    Tests:
     - deployment tests

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,32 +3,57 @@ pragma solidity ^0.8.0;
 
 contract Token {
 
+  uint private _totalSupply;
+  string private _tokenName;
+  string private _tokenSymbol;
+  uint8 private _decimals;
+  address public owner;
+  mapping (address => uint) private balances;
+
+  constructor(){
+    _totalSupply = 100_000;
+    _tokenSymbol = "LEO";
+    _tokenName = "Leocode Token";
+    _decimals = 18;
+    balances[msg.sender] = totalSupply();
+    owner = msg.sender;
+  }
+
   event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
   event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-  function name() public pure returns (string memory){
-    return "Leocode Token";
+  function name() public view returns (string memory){
+    return _tokenName;
   }
 
-  function symbol() public pure returns (string memory){
-    return "LEO";
+  function symbol() public view returns (string memory){
+    return _tokenSymbol;
   }
 
-  function decimals() public pure returns (uint8){
-    return 18;
+  function decimals() public view returns (uint8){
+    return _decimals;
   }
 
   function totalSupply() public view returns (uint256){
-    return 100_000 * (this.decimals() * 10);
+    return _totalSupply * ( 10 ** decimals());
   }
 
   function balanceOf(address _owner) public view returns (uint256 balance){
-
+    return balances[_owner];
   }
 
   function transfer(address _to, uint256 _value) public returns (bool success){
+    require(_to != address(0), "Cannot transfer to zero address");
+    require(_value < 0, "Cannot transfer negative amount");
+    require(balances[msg.sender] >= _value, "Insufficient funds");
 
+    balances[msg.sender] -= _value;
+    balances[_to] += _value;
+    
+    emit Transfer(msg.sender, _to, _value);
+
+    return true;
   }
 
   function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,53 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
+import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
+import { Token } from "../typechain";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+const TOTAL_SUPPLY = 100_000;
+const DECIMALS = 18;
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+describe("Token", function () {
+  let token: Token;
+  let owner: SignerWithAddress;
+  let address1: SignerWithAddress;
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+  beforeEach(async () => {
+    const Token = await ethers.getContractFactory("Token");
+    token = await Token.deploy();
+    [owner, address1] = await ethers.getSigners();
+    await token.deployed();
   });
+
+  describe("Deployment", () => {
+    it("Should return token name", async () => {
+      expect(await token.name()).to.equal("Leocode Token");
+    });
+
+    it("Should return token symbol", async () => {
+      expect(await token.symbol()).to.equal("LEO");
+    });
+
+    it("Should return total supply", async () => {
+      expect(await token.totalSupply()).to.equal(
+        BigNumber.from(`${TOTAL_SUPPLY}`).mul(BigNumber.from(10).pow(DECIMALS))
+      );
+    });
+
+    it("Should set the right owner", async () => {
+      expect(await token.owner()).to.equal(owner.address);
+    });
+
+    it("Should assign all tokens to the owner", async () => {
+      const ownerBalance = await token.balanceOf(owner.address);
+      expect(await token.totalSupply()).to.equal(ownerBalance);
+    });
+
+    it("Should not have any assets at not owner address", async () => {
+      const address1Balance = await token.balanceOf(address1.address);
+      expect(await token.balanceOf(address1.address)).to.equal(address1Balance);
+    });
+  });
+
+  describe("Transactions", () => {});
 });
